1e976d2fd84d980f70a7edf19bb161cde274f3e0
wpmello <wagnermello903@gmail.com>
2022-05-11 19:41:36 -0300

    Added angular's pipe and creating a custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { StartComponent } from './star/star.component';
+import { ReplacePipe } from './pipe/replace.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
-    StartComponent
+    StartComponent,
+    ReplacePipe
   ],
   imports: [
     BrowserModule,
