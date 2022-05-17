8e5541cf10d1d8d4da30a8009bf6d6cff32de985
wpmello <wagnermello903@gmail.com>
2022-05-17 15:39:24 -0300

    Http request - DELETE

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -30,6 +30,16 @@ export class CourseListComponent implements OnInit {
     })
   }
 
+  deleteById(courseId: number): void {
+    this.courseService.deleteById(courseId).subscribe({
+      next: () => {
+        console.log('Deleted with sucess');
+        this.retrieveAll();
+      },
+      error: err => console.log('Error', err)
+    })
+  }
+
   set filter(value: string) {
       this._filterBy = value;
 
--- src/app/courses/course.service.ts
+++ src/app/courses/course.service.ts
@@ -28,6 +28,10 @@ export class CourseService {
       return this.httpCliente.post<Course>(`${this.coursesUrl}`, course);
     }
   }
+
+  deleteById(id: number): Observable<any> {
+    return this.httpCliente.delete<any>(`${this.coursesUrl}/${id}`);
+  }
 }
 
 var COURSES: Course[] = [
