7e17233fcdb493b46cd09c155b97f893ae02712d
wpmello <wagnermello903@gmail.com>
2022-05-17 19:42:59 -0300

    Separating the modules - folder 'core' Error404Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { StartComponent } from './shared/component/star/star.component';
-import { ReplacePipe } from './shared/pipe/replace.pipe';
-import { NavBarComponet } from './core/component/nav-bar/nav-bar.component';
-import { Error404Component } from './error-404/error-404.component';
 import { CourseModule } from './courses/course.module';
 import { CoreModule } from './core/component/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Error404Component,
   ],
   imports: [
     BrowserModule,
@@ -25,9 +19,6 @@ import { CoreModule } from './core/component/core.module';
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
-      },
-      {
-        path: '**', component: Error404Component
       }
     ])
 
--- src/app/core/component/core.module.ts
+++ src/app/core/component/core.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { Error404Component } from "./error-404/error-404.component";
 import { NavBarComponet } from "./nav-bar/nav-bar.component";
 
 @NgModule({
   declarations: [
-    NavBarComponet
+    NavBarComponet,
+    Error404Component
   ],
   imports:[
-    RouterModule
+    RouterModule.forChild([
+      {
+        path: '**', component: Error404Component
+      }
+  ])
   ],
   exports: [
     NavBarComponet
